fddd18ab74ebd9b458d20669e3791c438efcdc3a
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-02-17 21:41:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string === "I love you, Grandma." {
+  if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 
-  else if string === string.toLowerCase() {
+  else if (string === string.toLowerCase()) {
     return "I can't hear you!"
   }
-  else if string === string.toUpperCase() {
+  else if (string === string.toUpperCase()) {
     return "YES INDEED!"
   }
 }
